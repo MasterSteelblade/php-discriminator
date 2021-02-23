1e1b9c2d5e0f4d19039dfe6990a16bbf9225eb70
MasterSteelblade <mastersteelblade@protonmail.com>
2021-02-23 15:55:54 +0000

    Fixed test failure due to deprecated assertion

--- tests/DiscriminatorTest.php
+++ tests/DiscriminatorTest.php
@@ -50,7 +50,7 @@ final class DiscriminatorTest extends \PHPUnit\Framework\TestCase {
 
     public function testGenerate() {
         $disc = Discriminator::generate();
-        $this->assertIsInt('int', $disc);
+        $this->assertIsInt($disc);
     }
 }
 
